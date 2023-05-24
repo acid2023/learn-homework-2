0ebdc6530404e6a8a8dff4812e7af23f904e84e6
Sergey Kuzmin <s.kuzmin@mail.ru>
2023-05-24 09:10:08 +0800

    homework

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,28 +1,37 @@
+from datetime import datetime, timedelta, date
 """
 Домашнее задание №2
-
+ 
 Дата и время
-
+ 
 1. Напечатайте в консоль даты: вчера, сегодня, 30 дней назад
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
-
+ 
 """
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-
-
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    thirty_days_ago = today - timedelta(days=30)
+    
+    print(today)
+    print(yesterday)
+    print(thirty_days_ago)
+ 
 def str_2_datetime(date_string):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-
+    str2_datetime = datetime.now()
+    str_date = str2_datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    return  str_date
+ 
 if __name__ == "__main__":
     print_days()
     print(str_2_datetime("01/01/20 12:10:03.234567"))
